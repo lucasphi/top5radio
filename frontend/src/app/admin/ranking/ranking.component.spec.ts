ff476ece5a70f676042d0d05cfa6c8ebbd893959
Lucas <lucasphi@gmail.com>
2020-01-07 02:52:42 -0800

    Fixing tests

--- frontend/src/app/admin/ranking/ranking.component.spec.ts
+++ frontend/src/app/admin/ranking/ranking.component.spec.ts
@@ -1,15 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Injectable } from '@angular/core';
+
+import { of } from 'rxjs/internal/observable/of';
 
 import { RankingComponent } from './ranking.component';
+import { Music } from 'src/app/shared/models/music.model';
+import { User } from 'src/app/shared/models/user.model';
+import { RankingService } from './ranking.service';
+
+@Injectable()
+class RankingServiceMock {
+  loadTopSongs(): any {
+    return of([] as Music[]);
+  }
+  loadUsers(): any {
+    return of([] as User[]);
+  }
+}
 
 describe('RankingComponent', () => {
   let component: RankingComponent;
   let fixture: ComponentFixture<RankingComponent>;
+  let mockRankingService: RankingServiceMock;
 
   beforeEach(async(() => {
+    mockRankingService = new RankingServiceMock();
     TestBed.configureTestingModule({
       declarations: [ RankingComponent ]
     })
+    .overrideProvider(RankingService, { useValue: mockRankingService })
     .compileComponents();
   }));
 
